0ad7297a813287975582776f2f38d9831c9beb5a
laurabbity915 <laura_yuen915@outlook.com>
2023-02-02 16:39:57 +0000

    add API key and URL

---
+++ script.js
@@ -0,0 +1,12 @@
+// add API key be a varriable
+var APIkey = "zk4kTyVkPQ4JiiAjiDejZT8AgjgH2CSR";
+
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey" + APIkey;
+
+//display the search info of the New York Times Article Search API
+// function displaySearchInfo(){
+
+//     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey"
+// }
+
+console.log(queryURL);
