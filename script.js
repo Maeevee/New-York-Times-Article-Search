90b674d9fb40f1fc604e1ddf7f5e6ab820aa0de5
laurabbity915 <laura_yuen915@outlook.com>
2023-02-02 20:44:24 +0000

    add AJAX call

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 // add API key be a varriable
 var APIkey = "zk4kTyVkPQ4JiiAjiDejZT8AgjgH2CSR";
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey" + APIkey;
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=" + APIkey;
 
 //display the search info of the New York Times Article Search API
 // function displaySearchInfo(){
 
 //     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey"
 // }
+//create a Ajax call
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response){
+     console.log(response.response.docs);
 
-console.log(queryURL);
+});
